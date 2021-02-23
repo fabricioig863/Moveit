0ef45dc5390db9911ac0359a1bb45749386e0b5b
Fabricio <fabricioig863@gmail.com>
2021-02-23 16:46:32 -0300

    countdown completo

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return(
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/fabricioig863.png" alt="Eu" />
+      <div>
+        <strong>Fabricio Ignacio de Espindola</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
-import { ExperiencieBar } from "./components/ExperienceBar";
+import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperiencieBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
+
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
 
     <ExperiencieBar />
+
+    <section>
+      <div>
+        <Profile />
+        <CompletedChallenges />
+        <Countdown />
+      </div>
+      <div>
+
+      </div>
+    </section>
   </div>
   )
 }
