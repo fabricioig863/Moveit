882f4889ec40074898ac9cbd31b1be9c30b1f5aa
Fabricio <fabricioig863@gmail.com>
2021-02-23 11:59:26 -0300

    inicializando projeto moveit com nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperiencieBar } from "./components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+
+    <ExperiencieBar />
+  </div>
+  )
+}
